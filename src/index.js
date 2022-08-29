6dc9e94fbc4f77dfb4cb023761204f29527ac431
Hyerin Joo <candy980426@gmail.com>
2022-08-29 20:08:53 +0900

    ✨ 로그인 페이지에서 메인 페이지로 Routing추가

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/reset.css";
 import "./styles/common.css";
-import Login from "./pages/Login/Login";
-import Main from "./pages/Main/Main";
+import Router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(<Login />);
-root.render(<Main />);
+root.render(<Router />);
