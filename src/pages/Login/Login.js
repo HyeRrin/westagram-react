665d9b7f24d71750f0516ad2d2deb980e315a49d
Hyerin Joo <candy980426@gmail.com>
2022-09-11 21:33:28 +0900

    ♻️ 유효성 검사에 따른 버튼 스타일 변경 코드 개선

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,7 +5,6 @@ import "../Login/Login.scss";
 const Login = () => {
   const [userId, setUserId] = useState("");
   const [userPw, setUserPw] = useState("");
-  const [disabled, setDisabled] = useState(true);
   const navigate = useNavigate();
   const saveUserId = (event) => {
     setUserId(event.target.value);
@@ -13,10 +12,7 @@ const Login = () => {
   const saveUserPw = (event) => {
     setUserPw(event.target.value);
   };
-  const handleDisabled = () => {
-    let validation = userId.includes("@") && userPw.length > 4;
-    return validation ? setDisabled(false) : setDisabled(true);
-  };
+  const isInputValid = userId.includes("@") && userPw.length > 0;
 
   const checkSignIn = (e) => {
     e.preventDefault();
@@ -59,7 +55,6 @@ const Login = () => {
             value={userId}
             className="login-id"
             onChange={saveUserId}
-            onKeyUp={handleDisabled}
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
@@ -67,18 +62,17 @@ const Login = () => {
             value={userPw}
             className="login-pw"
             onChange={saveUserPw}
-            onKeyUp={handleDisabled}
             type="password"
             placeholder="비밀번호"
           />
           <button
             style={
-              disabled === true
-                ? { backgroundColor: "rgb(0, 149, 246, 0.3)" }
-                : { backgroundColor: "rgb(0, 149, 246, 1)" }
+              isInputValid
+                ? { backgroundColor: "rgb(0, 149, 246, 1)" }
+                : { backgroundColor: "rgb(0, 149, 246, 0.3)" }
             }
             className="login-btn"
-            disabled={disabled}
+            disabled={isInputValid}
             onClick={checkSignIn}
           >
             로그인
