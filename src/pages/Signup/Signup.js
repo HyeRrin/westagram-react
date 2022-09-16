86fb4d597f31c8e2f7e5d4f4e3bd44d05fc137ac
Hyerin Joo <candy980426@gmail.com>
2022-09-16 23:27:40 +0900

    ✨ 회원가입 기능 추가

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,42 +3,66 @@ import { useNavigate, Link } from "react-router-dom";
 import "../Signup/Signup.scss";
 
 const Signup = () => {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const saveEmail = (event) => {
-    setEmail(event.target.value);
+  // const [email, setEmail] = useState("");
+  // const [name, setName] = useState("");
+  // const [nickname, setNickname] = useState("");
+  // const [password, setPassword] = useState("");
+
+  // const saveEmail = (event) => {
+  //   setEmail(event.target.value);
+  // };
+  // const saveName = (event) => {
+  //   setName(event.target.value);
+  // };
+  // const saveNickname = (event) => {
+  //   setNickname(event.target.value);
+  // };
+  // const savePassword = (event) => {
+  //   setPassword(event.target.value);
+  // };
+
+  const [imgFile, setImgFile] = useState("");
+  const imgRef = useRef();
+
+  const [inputValue, setInputValue] = useState({
+    email: "",
+    name: "",
+    nickname: "",
+    password: "",
+  });
+
+  const inputChange = (e) => {
+    setInputValue({ ...inputValue, [e.target.name]: e.target.value });
   };
-  const saveName = (event) => {
-    setName(event.target.value);
-  };
-  const saveNickname = (event) => {
-    setNickname(event.target.value);
-  };
-  const savePassword = (event) => {
-    setPassword(event.target.value);
-  };
+
+  const { email, name, nickname, password } = inputValue;
+
   const isInputValid =
     email.includes("@") &&
     password.length > 0 &&
     name.length > 0 &&
     nickname.length > 0;
 
+  const signupForm = document.getElementById("signupForm");
+
   const checkSignUp = (e) => {
     e.preventDefault();
-    fetch("http://10.58.3.201:3000/auth/signup", {
+    // const formData = new FormData();
+    // formData.append("email", email);
+    // formData.append("name", name);
+    // formData.append("nickname", nickname);
+    // formData.append("password", password);
+    // formData.append("profileImg", imgRef.current.files[0]);
+    // for (const keyValue of formData) console.log(keyValue);
+
+    fetch("http://10.58.4.101:8000/users/signup", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json;charset=utf-8",
+        enctype: "multipart/form-data",
+        // "Content-Type": "multipart/form-data",
       },
-      body: JSON.stringify({
-        email: email,
-        name: name,
-        nickname: nickname,
-        password: password,
-      }),
+      body: new FormData(signupForm),
     })
       .then((response) => {
         if (response.ok === true) {
@@ -50,31 +74,23 @@ const Signup = () => {
         alert(error);
       })
       .then((data) => {
-        if (data.message === "login success") {
-          localStorage.setItem("TOKEN", data.accessToken);
+        console.log(data);
+        if (data.message === "추석 특공대 화이팅!") {
           alert("회원가입 성공");
-          navigate("/login-joo");
+          // navigate("/");
         } else {
           alert("회원가입 실패");
         }
       });
   };
 
-  const [imgFile, setImgFile] = useState("");
-  const imgRef = useRef();
-
-  // 프로필 이미지 추가의 onChange
-  const saveImgFile = () => {
-    const file = imgRef.current.files[0];
+  const previewImg = () => {
     const reader = new FileReader();
+    const file = imgRef.current.files[0];
+    reader.readAsDataURL(file);
     reader.onloadend = () => {
       setImgFile(reader.result);
     };
-    reader.readAsDataURL(file);
-
-    const formData = new FormData(); // FormData{}
-    formData.append("profileImg", file);
-    for (const keyValue of formData) console.log(keyValue);
   };
 
   return (
@@ -83,48 +99,56 @@ const Signup = () => {
         <h1 className="signup-title">Westagram</h1>
         <img
           className="signup-profileImg"
-          src={
-            imgFile ? imgFile : `${process.env.PUBLIC_URL}/images/icon/user.png`
-          }
+          src={imgFile ? imgFile : `/images/icon/user.png`}
           alt="프로필 이미지"
         />
-        <form className="form-signup">
+        <form
+          className="form-signup"
+          id="signupForm"
+          onChange={inputChange}
+          onSubmit={checkSignUp}
+        >
           <label className="signup-profileImg-label" htmlFor="profileImg">
             프로필 이미지 추가
           </label>
           <input
             className="signup-profileImg-input"
+            name="profileImg"
             type="file"
             accept="image/*"
             id="profileImg"
-            onChange={saveImgFile}
+            onChange={previewImg}
             ref={imgRef}
           />
           <input
-            value={email}
+            // value={email}
+            name="email"
             className="signup-email"
-            onChange={saveEmail}
+            // onChange={saveEmail}
             type="text"
             placeholder="이메일 주소"
           />
           <input
-            value={name}
+            //value={name}
+            name="name"
             className="signup-name"
-            onChange={saveName}
+            // onChange={saveName}
             type="text"
-            placeholder="성명"
+            placeholder="이름"
           />
           <input
-            value={nickname}
+            // value={nickname}
+            name="nickname"
             className="signup-id"
-            onChange={saveNickname}
+            // onChange={saveNickname}
             type="text"
-            placeholder="사용자 이름"
+            placeholder="닉네임"
           />
           <input
-            value={password}
+            // value={password}
+            name="password"
             className="signup-pw"
-            onChange={savePassword}
+            // onChange={savePassword}
             type="password"
             placeholder="비밀번호"
           />
@@ -136,8 +160,7 @@ const Signup = () => {
                 : { backgroundColor: "rgb(0, 149, 246, 0.3)" }
             }
             className="signup-btn"
-            disabled={isInputValid}
-            onClick={checkSignUp}
+            disabled={!isInputValid}
           >
             회원가입
           </button>
